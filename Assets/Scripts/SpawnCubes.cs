a8394570a6a284dca9964316b6a1dded3efb3889
angar44a <antoni.galardo2000@gmail.com>
2021-03-19 23:32:46 +0100

    Project2

---
+++ Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject icecube;
+    public GameObject spawnpoint;
+    public AudioClip cling;
+    private float spawnTime = 3f;
+    void Start()
+    {
+        InvokeRepeating("SpawnCube", spawnTime, spawnTime);
+
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = cling;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnCube()
+    {
+        
+        Instantiate(icecube, spawnpoint.transform.position, Quaternion.identity);
+    }
+    void OnCollisionEnter()
+    {
+        GetComponent<AudioSource>().Play();
+
+    }
+}
