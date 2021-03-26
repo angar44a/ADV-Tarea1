104c873cd3e08946bdafe6a1eed32681b482d5cf
angar44a <antoni.galardo2000@gmail.com>
2021-03-26 20:33:09 +0100

    State Machines
    also deletes some unused stuff

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator anim;
+    private void OnTriggerEnter(Collider collision)
+    {
+        anim = collision.gameObject.GetComponent<Animator>();
+        anim.SetBool("abierto", !(anim.GetBool("abierto")  )  );
+    }
+}
