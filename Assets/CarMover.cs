2cbfe8c0c48aa1a90d790918697d92bee628a9e6
angar44a <antoni.galardo2000@gmail.com>
2021-05-21 19:13:56 +0200

    The Final Project

---
+++ Assets/CarMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMover : MonoBehaviour
+{
+    public float speed = 1f;
+    //private Animator _anim;
+    //public Rigidbody2D _rb2d;
+    public GameObject _player;
+    public GameObject splat;
+    private Vector3 direction;
+    public float hitrange;
+    void Start()
+    {
+        //direction = Vector3.zero;
+        //_anim = GetComponent<Animator>();
+        //_rb2d = GetComponent<Rigidbody2D>();
+
+        Invoke("DestroyThis", 4.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_anim.SetFloat("speed", Mathf.Abs(direction.magnitude));
+        //transform.position = transform.forward * Time.deltaTime * speed;
+        transform.Translate(Vector3.forward * 1 * Time.deltaTime * speed);
+    }
+
+    void DestroyThis() { Destroy(this.gameObject); }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject instance;
+        instance = Instantiate(splat, new Vector3(this.transform.position.x, this.transform.position.y + 1.0f, this.transform.position.z), Quaternion.Euler(Vector3.left * 90));
+        instance.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 1.0f, this.transform.position.z);
+        Destroy(other.gameObject);
+        
+    }
+}
---
+++ Assets/CarSystem.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSystem : MonoBehaviour
+{
+    public GameObject car1;
+    public GameObject car2;
+    public GameObject car3;
+    public GameObject splat;
+    public GameObject _player;
+    public GameObject death_screen;
+    private GameObject cartype;
+    private int RNG;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CarSpawning", 3.0f, 2.0f);
+        cartype = car1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_player == null) { death_screen.SetActive(true); }
+    }
+
+    void CarSpawning()
+    {
+        GameObject instance;
+        RNG = Random.Range(0, 4);
+        if(RNG == 0)
+        {
+            RNG = Random.Range(0, 3);
+            if (RNG == 0) cartype = car1;
+            if (RNG == 1) cartype = car2;
+            if (RNG == 2) cartype = car3;
+            instance = Instantiate(cartype, new Vector3(70.0f, 10.6f, -15.5f), Quaternion.Euler(Vector3.up * 270));
+            instance.transform.position = new Vector3(70.0f, 10.6f, -15.5f);
+            RNG = Random.Range(0, 3);
+            if (RNG == 0) {
+                RNG = Random.Range(0, 3);
+                if (RNG == 0) cartype = car1;
+                if (RNG == 1) cartype = car2;
+                if (RNG == 2) cartype = car3;
+                instance = Instantiate(cartype, new Vector3(76.0f, 10.6f, -20.0f), Quaternion.Euler(Vector3.up * 270));
+                instance.transform.position = new Vector3(76.0f, 10.6f, -20.0f);
+            }
+        }
+        else if (RNG == 1)
+        {
+            RNG = Random.Range(0, 3);
+            if (RNG == 0) cartype = car1;
+            if (RNG == 1) cartype = car2;
+            if (RNG == 2) cartype = car3;
+            instance = Instantiate(cartype, new Vector3(-55.0f, 10.6f, -25.6f), Quaternion.Euler(Vector3.up * 90));
+            instance.transform.position = new Vector3(-55.0f, 10.6f, -25.6f);
+            RNG = Random.Range(0, 3);
+            if (RNG == 0)
+            {
+                RNG = Random.Range(0, 3);
+                if (RNG == 0) cartype = car1;
+                if (RNG == 1) cartype = car2;
+                if (RNG == 2) cartype = car3;
+                instance = Instantiate(cartype, new Vector3(-50.0f, 10.6f, -30.5f), Quaternion.Euler(Vector3.up * 90));
+                instance.transform.position = new Vector3(-50.0f, 10.6f, -30.5f);
+            }
+        }
+        else if (RNG == 2)
+        {
+            RNG = Random.Range(0, 3);
+            if (RNG == 0) cartype = car1;
+            if (RNG == 1) cartype = car2;
+            if (RNG == 2) cartype = car3;
+            RNG = Random.Range(0, 3);
+            instance = Instantiate(cartype, new Vector3(3.0f, 10.6f, 5.0f), Quaternion.Euler(Vector3.up * 180));
+            instance.transform.position = new Vector3(3.0f, 10.6f, 5.0f);
+            RNG = Random.Range(0, 3);
+            if (RNG == 0)
+            {
+                RNG = Random.Range(0, 3);
+                if (RNG == 0) cartype = car1;
+                if (RNG == 1) cartype = car2;
+                if (RNG == 2) cartype = car3;
+                instance = Instantiate(cartype, new Vector3(7.3f, 10.6f, 10.0f), Quaternion.Euler(Vector3.up * 180));
+                instance.transform.position = new Vector3(7.3f, 10.6f, 10.0f);
+            }
+        }
+        else if (RNG == 3)
+        {
+            RNG = Random.Range(0, 3);
+            if (RNG == 0) cartype = car1;
+            if (RNG == 1) cartype = car2;
+            if (RNG == 2) cartype = car3;
+            instance = Instantiate(cartype, new Vector3(17.4f, 10.6f, -45.5f), Quaternion.Euler(Vector3.up));
+            instance.transform.position = new Vector3(17.4f, 10.6f, -45.5f);
+            RNG = Random.Range(0, 3);
+            if (RNG == 0)
+            {
+                RNG = Random.Range(0, 3);
+                if (RNG == 0) cartype = car1;
+                if (RNG == 1) cartype = car2;
+                if (RNG == 2) cartype = car3;
+                instance = Instantiate(cartype, new Vector3(13.0f, 10.6f, -45.5f), Quaternion.Euler(Vector3.up));
+                instance.transform.position = new Vector3(13.0f, 10.6f, -50.5f);
+            }
+        }
+    }
+
+}
